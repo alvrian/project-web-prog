1b88858024a46550f08b3de58f0074b5118b8730
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-11-18 14:56:30 +0700

    update:nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,9 +9,6 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <!-- isi di sini -->
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      </form>
     </div>
   </div>
 </nav>  
