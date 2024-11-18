ac52020416b63a861f3c5a5bf9cc56eeee124af8
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-11-18 14:42:28 +0700

    create: view Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <x-navbar/>
+    <div class = "d-flex flex-column justify-content-center align-items-center gap-4">
+        <div class = "d-flex flex-column justify-content-center align-items-center">
+            <div style = "background-color: #C6CE9D;width: 90vw;height: 30rem;border-radius:12px;color:white;margin:1rem;padding:2rem;">
+                <h1 style = "font-weight:bold;">LOGO</h1>
+                <span>Hello im under water please help me</span>
+            </div>
+        </div>
+        <span style = "width: 90vw;font-weight:bold;font-size: 24px;">Work Together with Us</span>
+        <div class = "d-flex justify-content-between" style = "width: 90vw;">
+                <div class="card d-flex justify-content-center align-items-center" style="width: 21rem;height: 28rem;color:white;background-color:#43553D;border-radius:12px">
+                    <img src="..." class="card-img-overlay" alt="...">
+                    <span>Restaurant</span>
+                </div>
+                <div class="card d-flex justify-content-center align-items-center" style="width: 21rem;height: 28rem;color:white;background-color:#43553D;border-radius:12px;">
+                    <img src="..." class="card-img-overlay" alt="...">
+                    <span>Restaurant</span>
+                </div>
+                <div class="card d-flex justify-content-center align-items-center" style="width: 21rem;height: 28rem;color:white;background-color:#43553D;border-radius:12px;">
+                    <img src="..." class="card-img-overlay" alt="...">
+                    <span>Restaurant</span>
+                </div>
+            </div>
+    </div>
+</x-layout>
