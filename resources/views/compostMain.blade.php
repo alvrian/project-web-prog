6fe05f745dd9dcea69b50b38f968f8048b454810
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-11-23 20:52:10 +0700

    add: market

--- resources/views/compostMain.blade.php
+++ resources/views/compostMain.blade.php
@@ -4,12 +4,14 @@
         <div class = "d-flex flex-column justify-content-center align-items-center" style = "background-color: #EBCF7B;height:20rem">
             <h2 style = "color:white;font-weight:600;">Looking for Something?</h2>
             <form class="d-flex" role="search" method = "GET" action = "/compost">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style = "width: 32rem;border-radius:15px;margin-top:1rem;" name = "search"/>
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style = "width: 32rem;border-radius:12px;margin-top:1rem;" name = "search"/>
             </form>
         </div>
         <div style ="padding: 2rem">
-            <span style = "font-size: 18px;font-weight:500;">Restaurant Around You</span>
-            
+            <span style = "font-size: 20px;font-weight:600">Restaurants Around You</span>
+            <div class = "card">
+                
+            </div>
         </div>
     </div>
 </x-layout>
