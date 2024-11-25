f35b123fe45188cf1ffb4b3321af054b0d9211d7
tiffany <tiffanylumengkewas@gmail.com>
2024-11-25 15:12:24 +0700

    init model and seeder

---
+++ app/Models/WasteLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WasteLog extends Model
+{
+    use HasFactory;
+}
