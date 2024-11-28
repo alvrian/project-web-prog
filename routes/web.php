e3d9b38da5ae86f1e97ae145724a92f70f878fd8
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-11-28 21:18:02 +0700

    adding laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\RestaurantController;
-use App\Http\Controllers\CompostController;
-use App\Http\Controllers\FarmerController;
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\WasteLogController;
 
-Route::get("/", [HomeController::class, 'index']);
-Route::get("/market", [HomeController::class, 'market']);
-Route::get("/aboutUs", [HomeController::class, 'aboutUS']);
-
-Route::prefix('restaurant')->group(function () {
-    Route::get('/', [RestaurantController::class, 'index'])->name('restaurant.index');
-
-    Route::get('/create-waste-log', [WasteLogController::class, 'create'])->name('waste_log.create');
-    Route::post('/create-waste-log', [WasteLogController::class, 'store'])->name('waste_log.store');
-
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get("/compost", [CompostController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("/farmer", [FarmerController::class, 'index']);
-
-Route::prefix('account')->group(function () {
-    Route::get("/", [AccountController::class, 'index']);
-    Route::get("/point", [AccountController::class, 'point']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-
+require __DIR__.'/auth.php';
