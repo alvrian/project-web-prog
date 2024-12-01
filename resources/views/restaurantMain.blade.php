ddaead554d36fbec912095233229a2dda9bd7ba4
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-01 21:39:50 +0700

    maap langsung push

--- resources/views/restaurantMain.blade.php
+++ resources/views/restaurantMain.blade.php
@@ -1,4 +1,13 @@
 <x-layout>
     <x-navbar/>
-    Hello, ini page main restaurant
+    <div style = "padding: 1rem 2rem;">
+        <div class = "d-flex" style = "width: 100%;height: 30vh; background-color: red;">
+            <div style = "width: 60%;background-color:blue;">
+
+            </div>
+            <div style = "width: 35%;background-color: green;">
+
+            </div>
+        </div>
+    </div>
 </x-layout>
