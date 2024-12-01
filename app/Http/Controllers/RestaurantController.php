4780ebd8524d59b15d063e9a407079af8a305e39
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-01 23:35:27 +0700

    sisa kosmetik

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PickupSchedule;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class RestaurantController extends Controller
 {
     public function index (){
-        return view("restaurantMain");
+        $user_id = auth()->user()->id;
+        $data = PickupSchedule::where('SenderRestaurantOwnerID', $user_id)->where('Status', 'Scheduled')->orderBy('ScheduledDate', 'asc')->get();
+        $today = Carbon::today(); 
+
+        foreach ($data as $item) {
+            $item->FormattedScheduledDate = Carbon::parse($item->ScheduledDate)->format('F, d Y, h:i A');
+        }
+
+        $dataToday = $data->filter(function ($item) use ($today) {
+            return Carbon::parse($item->ScheduledDate)->toDateString() === $today->toDateString();
+        });
+        return view("restaurantMain", compact("data", "dataToday"));
     }
 }
--- resources/views/restaurantMain.blade.php
+++ resources/views/restaurantMain.blade.php
@@ -8,10 +8,10 @@
           <div class="d-flex justify-content-center align-items-center"
             style="width: 40%;height: 100%;font-size: 18px;font-weight: 600;">This Month</div>
           <div style="width: 60%;height: 100%;overflow-x:hidden;overflow-y: auto;">
-            <ul class="list-group list-group-flush">
-              @for($i = 0; $i < 10; $i++)
-          <li class="list-group-item">An item</li>
-              @endfor
+            <ul class="list-group list-group-flush" style = "width: 75%;">  
+            @foreach ($data as $d)
+                <li class="list-group-item">&centerdot; {{$d->FormattedScheduledDate}}</li>
+              @endforeach
             </ul>
           </div>
         </div>
@@ -20,20 +20,27 @@
         <div
           style="height: 100%;width: 100%;border-radius: 12px;border: 2px solid #b8b8b8;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.1);padding: 1rem;">
           <span style="font-size: 18px;font-weight: 600;">Today</span><br>
-          aaaaaaaaaaa
+          <div style="width: 60%;height: 100%;overflow-x:hidden;overflow-y: auto;">
+            <ul class="list-group list-group-flush" style = "width: 100%;">  
+              @foreach ($dataToday as $d)
+                <li class="list-group-item">&centerdot; {{ \Carbon\Carbon::parse($d->ScheduledDate)->format('h:i A') }}</li>
+              @endforeach
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </div><br>
+    
+    <h5>Lanjut isi sini apus nanti</h5>
+
     <style>
       ::-webkit-scrollbar {
         width: 8px;
       }
-
       ::-webkit-scrollbar-thumb {
         background-color: #888;
         border-radius: 10px;
       }
-
       ::-webkit-scrollbar-thumb:hover {
         background-color: #555;
       }
@@ -43,7 +50,6 @@
         border-radius: 10px;
       }
     </style>
-
-    <h5>Lanjut isi sini apus nanti</h5>
   </div>
+  
 </x-layout>
