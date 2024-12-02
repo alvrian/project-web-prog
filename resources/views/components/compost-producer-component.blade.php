2b59ac2eee9b5548cba9cfc0a22879d0a4b9505d
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-03 01:07:49 +0700

    cp-2

--- resources/views/components/compost-producer-component.blade.php
+++ resources/views/components/compost-producer-component.blade.php
@@ -1,5 +1,17 @@
-<div>
-    <div>
-        <span>Hello world</span>
+<div class="row" style="height: 42vh;">
+  <div class="col-12 col-md-8" style="height: 100%;padding: 0 4px 0  4px;">
+    <div
+      style="width: 100%;border-radius: 12px;border: 2px solid #b8b8b8;box-shadow: 4px 7px 8px 0px rgba(163,163,163,0.1);height: 100%;padding:1rem;">
+      <div class = "d-flex justify-content-between">
+        <span style = "font-size:20px;font-weight:600;">Waste Pick up Schedule</span>
+        <span>January, 2024</span>
+      </div>
     </div>
+  </div>
+  <div class="col-12 col-md-4" style="height: 100%;padding: 0 4px 0  4px;">
+    <div
+      style="width: 100%;border-radius: 12px;border: 2px solid #b8b8b8;box-shadow: 4px 7px 8px 0px rgba(163,163,163,0.1);height: 100%;padding:1rem;">
+      <span style = "font-size:20px;font-weight:600;">Schedule a Pickup</span>
+    </div>
+  </div>
 </div>
--- resources/views/compostMain.blade.php
+++ resources/views/compostMain.blade.php
@@ -1,8 +1,7 @@
 <x-layout>
     <x-navbar/>
     <div style="padding: 1rem 5rem;font-family:&quot;Inter&quot;, serif;">
-    <h5 style = "font-size: 20px;font-weight:600;margin: 1rem 0;">Waste Pickup Schedule</h5>
     <x-compost-producer-component /><br>
-    
+
     </div>
 </x-layout>
