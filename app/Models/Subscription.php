56b9a797933226067306c12cf82dd47f133e5700
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-06 18:23:34 +0700

    mantap

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -10,7 +10,7 @@ class Subscription extends Model
     use HasFactory;
 
     protected $table = 'subscription';
-    protected $primaryKey = "SubscriberID";
+    protected $primaryKey = "SubscriptionID";
 
     protected $fillable = [
         'SubscriberID',
