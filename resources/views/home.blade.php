095b1490735b6e8fc82bbd2243e73becf89c5587
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-14 00:44:25 +0700

    adding temp guest homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,69 +31,93 @@
               sourced directly from local farms, ensuring every bite supports your community.
             </span>
             <img src={{ asset('images/farmByte-gray.png') }} style="position: absolute;bottom: 5%;left: 3.5%;">
+            @if(auth()->user())
             <a href="#">
               <button id="home-button-explore"
-                style="background-color: #D9D9D9;font-size: 18px;font-weight: 400;position: absolute;right: 3.5%;bottom: 5%;padding:4px 2rem;border-radius: 8px;border: none;">
+                style="background-color: #D9D9D9;font-size: 18px;font-weight: 400;position: absolute;right: 3.5%;bottom: 5%;padding:6px 2rem;border-radius: 8px;border: none;">
                 Explore
               </button>
             </a>
+            @else
+            <a href="/register">
+              <button id="home-button-explore"
+                style="background-color: #D9D9D9;font-size: 18px;font-weight: 400;position: absolute;right: 3.5%;bottom: 5%;padding:6px 2rem;border-radius: 8px;border: none;">
+                Register Now
+              </button>
+            </a>
+            @endif
           </div>
         </div>
       </div>
     </div>
   </div>
   <span style="width: 85vw;font-weight:bold;font-size: 24px;">What is FarmByte ?</span>
-  <div class="card d-flex flex-row"
-    style="width:85vw;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);background-color:#F0EEEF;border: none;">
-    <div class="card-body" style="flex: 1;color: #3C4B33;">
+  <div class="card d-flex"
+    style="width:85vw;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);background-color:#F0EEEF;border: none;height: 100%;">
+    <div class="d-flex flex-row"">
+      <div class=" card-body" style="flex: 1;color: #3C4B33;">
       Welcome to FarmByte, where we bridge the gap between farmers, compost producers, and restaurants to create a
       sustainable and mutually beneficial ecosystem. Our platform fosters collaboration, enabling farmers to supply
       fresh produce, compost producers to contribute eco-friendly solutions, and restaurants to source high-quality
       ingredients directly.
     </div>
     <div class="card-body d-flex flex-column justify-content-center dropend" style="flex:1;">
-      <h4 style="font-size: 28px;font-weight:bold;color: #3C4B33;margin-bottom:1rem;">Read More</h4>
+      <h4 style="font-size: 28px;font-weight:bold;color: #3C4B33;margin-bottom:1rem;">About Us</h4>
       <a href="/aboutUs"
         style="text-decoration: none;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);border-radius: 12px;">
         <button type="button" class="btn d-flex dropdown-toggle justify-content-between align-items-center"
           style="color:white;height:10vh;font-weight:500;font-size:18px;background-color: #C7D6E3;width:100%;border-radius: 12px;">
-          About Us
+          Read More
         </button>
       </a>
     </div>
   </div>
-  <span style="width: 85vw;font-weight:bold;font-size: 24px;">Work Together with Us</span>
-  <div class="d-flex justify-content-between " style="width: 85vw;">
+  @if(auth()->user() === null)
+  <span style="font-size: 22px;font-weight: 600;margin: 0 1rem;">What are You Waiting For?</span>
+  <a href = "/register" style = "text-decoration: none;">
+    <div class="dropend" style = "margin: 1rem;">
+      <button type="button" class="btn d-flex dropdown-toggle justify-content-between align-items-center"
+        style="color:white;height:10vh;font-weight:500;font-size:18px;background-color:rgb(237, 206, 114);width:100%;border-radius: 12px;">
+        Register Now
+      </button>
+    </div>
+  </a>
+  @endif
+  </div>
+  @if(auth()->user())
+    <span style="width: 85vw;font-weight:bold;font-size: 24px;">Work Together with Us</span>
+    <div class="d-flex justify-content-between " style="width: 85vw;">
     <a href="{{ route('restaurant.index') }}" style="text-decoration: none;">
       <div class="card d-flex justify-content-center align-items-center"
-        style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
-        <img src="{{ asset('images/home-restaurantLogo.png') }}"
-          class="d-flex justify-content-center align-items-center" alt="...">
-        <span style="padding: 1rem;font-size: 22px;font-weight:700;">
-          Restaurant
-        </span>
+      style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
+      <img src="{{ asset('images/home-restaurantLogo.png') }}"
+        class="d-flex justify-content-center align-items-center" alt="...">
+      <span style="padding: 1rem;font-size: 22px;font-weight:700;">
+        Restaurant
+      </span>
       </div>
     </a>
     <a href="/compost-producer" style="text-decoration: none;">
       <div class="card d-flex justify-content-center align-items-center"
-        style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
-        <img src="{{ asset('images/home-compostLogo.png') }}" class="d-flex justify-content-center align-items-center"
-          alt="...">
-        <span style="padding: 1rem;font-size: 22px;font-weight:700;text-align: center;">
-          Compost Producer
-        </span>
+      style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
+      <img src="{{ asset('images/home-compostLogo.png') }}" class="d-flex justify-content-center align-items-center"
+        alt="...">
+      <span style="padding: 1rem;font-size: 22px;font-weight:700;text-align: center;">
+        Compost Producer
+      </span>
       </div>
     </a>
     <a href="/farmer" style="text-decoration: none;">
       <div class="card d-flex justify-content-center align-items-center"
-        style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
-        <img src="{{ asset('images/home-farmLogo.png') }}" class="d-flex justify-content-center align-items-center"
-          alt="...">
-        <span style="padding: 1rem;font-size: 22px;font-weight:700;">
-          Farmer
-        </span>
+      style="width: 25vw;height: 30rem;color:white;background-color:#43553D;border-radius:12px;max-width: 400px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
+      <img src="{{ asset('images/home-farmLogo.png') }}" class="d-flex justify-content-center align-items-center"
+        alt="...">
+      <span style="padding: 1rem;font-size: 22px;font-weight:700;">
+        Farmer
+      </span>
       </div>
     </a>
-  </div>
+    </div>
+  @endif
   </div>
 </x-layout>
