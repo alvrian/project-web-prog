e22ba114edd17062bb0a6fadf88b1fe54436ab72
Alvrian Timotius H <108281792+alvrian@users.noreply.github.com>
2024-12-17 21:27:36 +0700

    add: home quick action

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,7 +86,7 @@
   </div>
   @if(auth()->user())
     <span style="width: 85vw;font-weight:bold;font-size: 24px;">Work Together with Us</span>
-    <div class="row" style="width: 85vw;">
+    <div class="row" style="width: 85vw;height: 30rem;">
       @if(auth()->user()->role == "restaurant_owner")
       <a href="{{ route('restaurant.index') }}" style="text-decoration: none;">
         <div class="card d-flex justify-content-center align-items-center"
@@ -99,10 +99,10 @@
         </div>
       </a>
       @elseif(auth()->user()->role == "compost_producer")
-      <div class = "col-4">
+      <div class = "col-4" style = "height: 100%;">
         <a href="/compost-producer" style="text-decoration: none;">
           <div class="card d-flex justify-content-center align-items-center"
-          style="height: 30rem;color:white;background-color:#43553D;border-radius:12px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
+          style="height: 100%;color:white;background-color:#43553D;border-radius:12px;box-shadow: 5px 7px 8px 0px rgba(163,163,163,0.17);">
           <img src="{{ asset('images/home-compostLogo.png') }}" class="d-flex justify-content-center align-items-center"
             alt="...">
           <span style="padding: 1rem;font-size: 22px;font-weight:700;text-align: center;">
@@ -111,17 +111,39 @@
           </div>
         </a>
       </div>
-      <div class = "col-8" style = "padding:10px  1rem;">
-      <span style = "font-size: 22px;font-weight: 600;display: block;">Welcome, </span>
-        <span style = "font-size: 24px;font-weight: 600;display: block;">{{auth()->user()->name}}</span>
-        <span style  = "font-size: 18px;color: rgba(60, 60, 60, 0.81);display:block;">{{auth()->user()->email}}</span><br>
-        <!-- <div class = "dropend">
-          <a href = "/compost-producer" style = "text-decoration: none;">
-            <button class = "btn dropdown-toggle" type = "button" style = "background-color: #EBCF7B;border: none;color: white;">
-              Go to Compost Portal
-            </button>
+      <div class="col-8" style="padding: 0 1rem; height: 100%; display: flex; flex-direction: column; justify-content: space-between;">
+        <div>
+          <span style="font-size: 22px; font-weight: 600; display: block;">Welcome, </span>
+          <span style="font-size: 24px; font-weight: 600; display: block;">{{ auth()->user()->name }}</span>
+          <span style="font-size: 18px; color: rgba(60, 60, 60, 0.81); display: block;">{{ auth()->user()->email }}</span>
+        </div>
+        <div class="row" style="position: relative;box-shadow: 5px 7px 8px 0px rgba(163, 163, 163, 0.17); padding: 3rem 1rem ;border: 2px solid #b8b8b8;border-radius: 12px;margin-left: 1px;">
+          <span style = "position: absolute;top: 5%;font-size: 24px;font-weight: 600;">Quick Action</span>
+          <div class="col" style = "border: 2px solid #b8b8b8;padding: 1rem;border-radius: 12px;margin: 20px;" id = "box-quick">
+            <a href = "/account" style = "text-decoration:none;color: black;">
+            <div class = "d-flex flex-column justify-content-center align-items-center">
+                <img  src = "{{ asset('images/account-quick.png') }}" style = "height: 100px;margin: 1rem;">
+                <span>account</span>
+            </div>
+            </a>
+          </div>
+          <div class="col" style="padding: 1rem;border: 2px solid #b8b8b8;border-radius: 12px;margin: 20px;" id = "box-quick">
+            <a href = "/compost-producer/composts" style = "text-decoration:none;color: black;">
+            <div class = "d-flex flex-column justify-content-center align-items-center">
+              <img  src = "{{ asset('images/catalog-quick.png') }}" style = "height: 100px;margin: 1rem;">
+              <span>My Catalog</span>
+            </div>
+            </a>
+          </div>
+          <div class="col" style="padding: 1rem;border: 2px solid #b8b8b8;border-radius: 12px;margin: 20px;" id = "box-quick">
+          <a href = "/compost-producer" style = "text-decoration:none;color: black;">
+          <div class = "d-flex flex-column justify-content-center align-items-center">
+              <img  src = "{{ asset('images/portal-quick.png') }}" style = "height: 100px;margin: 1rem;">
+              <span>Portal</span>
+            </div>
+          </div>
           </a>
-        </div> -->
+        </div>
       </div>
       @elseif(auth()->user()->role == "farmer")
       <a href="/farmer" style="text-decoration: none;">
@@ -138,4 +160,12 @@
     </div>
   @endif
   </div>
+  <style>
+  #box-quick {
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+  }#box-quick:hover {
+    background-color:rgba(216, 216, 216, 0.16);
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2);
+  }
+  </style>
 </x-layout>
